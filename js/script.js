9add2f6b95e015742cbfdb73b75c9c6547056644
Josericardodesouza <souzaricardo.professionaldev@gmail.com>
2024-02-20 10:41:04 -0300

    update JavaScript

--- js/script.js
+++ js/script.js
@@ -5,13 +5,28 @@ function calcular() {
 
     var altura = document.getElementById("entrada-altura").value;
 
-    let imc = peso / (altura * 2);
+    var total = peso / (altura * altura);
 
+    var imc = total.toFixed(1);
 
+    if (imc < 17) {
+        document.getElementById("saida").innerHTML = "Voce esta abaixo do peso"
+    } else if (imc >= 17.0 && imc <= 19.0) {
+        document.getElementById("saida").innerHTML = "Voce esta abaixo do peso"
+    } else if (imc >= 18.50 && imc <= 25.0) {
+        document.getElementById("saida").innerHTML = "Voce esta com o peso normal"
+    } else if (imc >= 25.01 && imc <= 30.0) {
+        document.getElementById("saida").innerHTML = "Voce esta acima do peso"
+    } else if (imc >= 30.0 && imc <= 35.0) {
+        document.getElementById("saida").innerHTML = "Obesidade 1"
+    } else if (imc >= 35.01 && imc <= 40.0) {
+        document.getElementById("saida").innerHTML = "Obesidade 2(severa)"
+    } else if (imc >= 40.1) {
+        document.getElementById("saida").innerHTML = "Obesidade 3(mórbida)"
+    }
 
+    
 
-    document.getElementById('saida').innerHTML = imc;
+    document.getElementById("saida-frase").innerHTML = "O seu IMC é de " + imc
 
-
-}
-
+    }
