8af40a474dc82dd5ad3fe0ce7176a4c6ed3f1af1
Josericardodesouza <souzaricardo.professionaldev@gmail.com>
2024-02-20 14:54:24 -0300

    update calccal

--- js/clrs.js
+++ js/clrs.js
@@ -1,22 +1,32 @@
 function calcularCal() {
-    var carboidratos = document.getElementById('ent-carboidratos').value;
-    var totCarboidratos = carboidratos * 4;
+    var carboidratos = document.getElementById("ent-carboidratos").value;
 
-    var proteinas =  document.getElementById('ent-proteinas').value;
-    var totProteinas = proteinas * 4;
+    var totCarboidratos = (carboidratos * 4);
 
-    var lipidios = document.getElementById('ent-lipidios').value;
-    var totLipidios = lipidios * 9;
+    var proteinas =  document.getElementById("ent-proteinas").value;
 
-    document.getElementById('saida-carb').innerHTML = carboidratos + "G carboidratos";
+    var totProteinas = (proteinas * 4);
 
-    document.getElementById('saida-carb2').innerText = carboidratos + "X 4 = " + totCarboidratos + "KCAL";
+    var lipidios = document.getElementById("ent-lipi").value;
 
-    document.getElementById('saida-prot').innerHTML = proteinas + "G proteínas";
+    var totLipidios = (lipidios * 9);
 
-    document.getElementById('saida-prot2').innerHTML = proteinas + "X 4 = " + totProteinas + "KCAL";
+    var total = (totCarboidratos + totProteinas + totLipidios);
 
-    document.getElementById('saida-lipi').innerHTML = lipidios + "G lipídios";
+    document.getElementById("saida-carb").innerHTML = carboidratos + "G carboidratos"
 
-    document.getElementById('saida-lipi2').innerHTML = lipidios + "X 9 = " + totLipidios + "KCAL";
+    document.getElementById("saida-carb2").innerText = carboidratos + "X 4 = " + totCarboidratos + "KCAL"
+
+    document.getElementById("saida-prot").innerHTML = proteinas + "G proteínas"
+
+    document.getElementById("saida-prot2").innerHTML = proteinas + "X 4 = " + totProteinas + "KCAL"
+
+    document.getElementById("saida-lipi").innerHTML = lipidios + "G lipídios"
+
+    document.getElementById("saida-lipi2").innerHTML = lipidios + "X 9 = " + totLipidios + "KCAL"
+
+    document.getElementById("saida-calc").innerHTML = carboidratos + " + " + proteinas + " + " + lipidios
+
+    document.getElementById("saida-total").innerHTML = "Calorias totais " + total
 }
+
