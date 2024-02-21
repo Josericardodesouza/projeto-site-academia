9c9bfe76f9291ef07ea97e20c326acbe8486c2b2
Josericardodesouza <souzaricardo.professionaldev@gmail.com>
2024-02-20 22:35:20 -0300

    Update geb.js

--- js/geb.js
+++ js/geb.js
@@ -1,41 +1,93 @@
+function calculoMasculino() {
+
+    var base = 66.47
+
+    var pesoInfor = document.getElementById("ent-peso").value
+    var totalPeso = (13.75 * pesoInfor);
+
+    var altInfor = document.getElementById("ent-altura").value
+    var totalAlt = (5 * altInfor);
+
+    var idadeInfor = document.getElementById("ent-idade").value
+    var totalIdade = (6.76 * idadeInfor);
+
+    var geb = (base + totalPeso + totalAlt - totalIdade);
+
+    document.getElementById("saidaGeb").innerHTML = "O total deu " + geb + " KCAL"
+
+}
+
+function calculoFeminino() {
+
+    var base = 655.1
+
+    var pesoInfor = document.getElementById("ent-peso").value
+    var totalPeso = (9.56 * pesoInfor);
+
+    var altInfor = document.getElementById("ent-altura").value
+    var totalAlt = (1.85 * altInfor);
+
+    var idadeInfor = document.getElementById("ent-idade").value
+    var totalIdade = (4.68 * idadeInfor);
+
+    var geb = (base + totalPeso + totalAlt - totalIdade);
+
+    document.getElementById("saidaGeb").innerHTML = "O total deu " + geb + " KCAL"
+}
+
+
+
+
+
+
 function calcularGEB () {
     var sexo = document.getElementById("sexo").value;
 
-    if (sexo === "masc") {
+    if (sexo==="masc") {
 
-        var base = 66.47
+        calculoMasculino()
 
-        var pesoInfor = document.getElementById("ent-peso").value
-        var totalPeso = (13.75 * pesoInfor);
-
-        var altInfor = document.getElementById("ent-altura").value
-        var totalAlt = (5 * altInfor);
-
-        var idadeInfor = document.getElementById("ent-idade").value
-        var totalIdade = (6.76 * idadeInfor);
-
-        var geb = (base + totalPeso + totalAlt - totalIdade);
-
-        document.getElementById("saidaGeb").innerHTML = "O total deu " + geb + " KCAL"
-
-    }
-
-    if (sexo === "fem") {
-
-        var base = 655.1
-
-        var pesoInfor = document.getElementById("ent-peso").value
-        var totalPeso = (9.56 * pesoInfor);
-
-        var altInfor = document.getElementById("ent-altura").value
-        var totalAlt = (1.85 * altInfor);
-
-        var idadeInfor = document.getElementById("ent-idade").value
-        var totalIdade = (4.68 * idadeInfor);
-
-        var geb = (base + totalPeso + totalAlt - totalIdade);
-
-        document.getElementById("saidaGeb").innerHTML = "O total deu " + geb + " KCAL"
+    } else {
+        calculoFeminino()
     }
 
 }
+
+//     if (sexo === "masc") {
+
+//         var base = 66.47
+
+//         var pesoInfor = document.getElementById("ent-peso").value
+//         var totalPeso = (13.75 * pesoInfor);
+
+//         var altInfor = document.getElementById("ent-altura").value
+//         var totalAlt = (5 * altInfor);
+
+//         var idadeInfor = document.getElementById("ent-idade").value
+//         var totalIdade = (6.76 * idadeInfor);
+
+//         var geb = (base + totalPeso + totalAlt - totalIdade);
+
+//         document.getElementById("saidaGeb").innerHTML = "O total deu " + geb + " KCAL"
+
+//     }
+
+//     if (sexo === "fem") {
+
+//         var base = 655.1
+
+//         var pesoInfor = document.getElementById("ent-peso").value
+//         var totalPeso = (9.56 * pesoInfor);
+
+//         var altInfor = document.getElementById("ent-altura").value
+//         var totalAlt = (1.85 * altInfor);
+
+//         var idadeInfor = document.getElementById("ent-idade").value
+//         var totalIdade = (4.68 * idadeInfor);
+
+//         var geb = (base + totalPeso + totalAlt - totalIdade);
+
+//         document.getElementById("saidaGeb").innerHTML = "O total deu " + geb + " KCAL"
+//     }
+
+// }
