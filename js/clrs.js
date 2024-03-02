117713510828eff3c14b83a6e5452903d01688cb
Josericardodesouza <souzaricardo.professionaldev@gmail.com>
2024-03-02 11:19:39 -0300

    update css

--- js/clrs.js
+++ js/clrs.js
@@ -13,20 +13,24 @@ function calcularCal() {
 
     var total = (totCarboidratos + totProteinas + totLipidios);
 
-    document.getElementById("saida-carb").innerHTML = carboidratos + "G carboidratos"
+    document.getElementById("saida-totalCal").innerHTML = total;
 
-    document.getElementById("saida-carb2").innerText = carboidratos + "X 4 = " + totCarboidratos + "KCAL"
+    document.getElementById("formCal").innerHTML = carboidratos + " + " + proteinas + " + " + lipidios;
 
-    document.getElementById("saida-prot").innerHTML = proteinas + "G proteínas"
+    document.getElementById("saida-totalCarb").innerHTML = carboidratos + "G carboidratos";
 
-    document.getElementById("saida-prot2").innerHTML = proteinas + "X 4 = " + totProteinas + "KCAL"
+    document.getElementById("formCarb").innerText = carboidratos + "X 4 = " + totCarboidratos + "KCAL"
 
-    document.getElementById("saida-lipi").innerHTML = lipidios + "G lipídios"
+    document.getElementById("saida-totalProt").innerHTML = proteinas + "G proteínas"
 
-    document.getElementById("saida-lipi2").innerHTML = lipidios + "X 9 = " + totLipidios + "KCAL"
+    document.getElementById("formProt").innerHTML = proteinas + "X 4 = " + totProteinas + "KCAL"
 
-    document.getElementById("saida-calc").innerHTML = carboidratos + " + " + proteinas + " + " + lipidios
+    document.getElementById("saida-totalLipi").innerHTML = lipidios + "G lipídios"
 
-    document.getElementById("saida-total").innerHTML = "Calorias totais " + total
+    document.getElementById("saida-formLipid").innerHTML = lipidios + "X 9 = " + totLipidios + "KCAL"
+
+   
+
+   
 }
 
