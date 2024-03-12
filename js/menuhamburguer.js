0f35f307bc5aeef1b53bf20e1894a4ec7dee1228
Josericardodesouza <souzaricardo.professionaldev@gmail.com>
2024-03-12 16:07:56 -0300

    centralizando elementos do menu no index.html

--- js/menuhamburguer.js
+++ js/menuhamburguer.js
@@ -1,7 +1,24 @@
-function cliqueMenu () {
-    if (menu.style.display=='block') {
-        menu.style.display = 'none'
-    } else {
-        menu.style.display = 'block'
+// function cliqueMenu () {
+//     if (menu.style.display=='block') {
+//         menu.style.display = 'none'
+//     } else {
+//         menu.style.display = 'block'
+//     }
+// }
+
+
+
+
+function cliqueMenu() {
+    var botao = document.getElementById("span-nav");
+    var container = document.getElementById("oculta");
+
+    botao.addEventListener('click', function(){
+        if (container.style.display == 'block') {
+            container.style.display = 'none';
+        } else {
+            container.style.display = 'block';
+        }
     }
+    )
 }
